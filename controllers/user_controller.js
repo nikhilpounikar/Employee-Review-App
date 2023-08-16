7c22d183658c7641329e9b6c8f652ec3d0d1e5f1
nikhilpounikar <nikhilpounikar@gmail.com>
2023-08-16 10:34:53 +0530

    User controller comments added

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,29 +1,28 @@
-const User = require("../models/User"); // requring user
+const User = require("../models/User"); // Requiring the User model
 
-// redering the singIN page
+// Rendering the sign-in page
 module.exports.signIn = function (req, res) {
-  return res.render("sign_in");
+  return res.render("sign_in"); // Render the sign-in page view
 };
 
-// creating the session, basically for logging In
+// Creating a session for logging in
 module.exports.login = async function (req, res) {
   // console.log(req.body);
-  req.flash("success", "You are logged In");
-  return res.redirect("/");
+  req.flash("success", "You are logged In"); // Display a success flash message
+  return res.redirect("/"); // Redirect to the home page
 };
 
-// This function is used for rendering the signUp page
-
+// Rendering the sign-up page
 module.exports.signUp = function (req, res) {
-  return res.render("sign_up");
+  return res.render("sign_up"); // Render the sign-up page view
 };
 
-// This fucntion is for creating the new user
+// Creating a new user
 module.exports.createUser = async function (req, res) {
   if (req.body.password != req.body.confirmPassword) {
-    //disply flash messages
-    req.flash("error", "Password should be equal to Confirm Password");
-    return res.redirect("back");
+    // If passwords don't match
+    req.flash("error", "Password should be equal to Confirm Password"); // Display an error flash message
+    return res.redirect("back"); // Redirect back to the previous page
   }
   let user = await User.findOne({ email: req.body.email });
   if (!user) {
@@ -34,81 +33,84 @@ module.exports.createUser = async function (req, res) {
       isAdmin: false,
     });
 
-    return res.redirect("/");
+    return res.redirect("/"); // Redirect to the home page
   }
-  req.flash("error", "User Already exits try login.");
-  return res.redirect("back");
+  req.flash("error", "User Already exists. Try logging in."); // Display an error flash message
+  return res.redirect("back"); // Redirect back to the previous page
 };
 
-// This fucniton is used for logging Out
+// Logging out
 module.exports.logOut = function (req, res, done) {
   return req.logout((err) => {
     if (err) {
       return done(err);
     }
-    req.flash("success", "Logged Out Sucessfully !");
-    return res.redirect("/users/sign-in");
+    req.flash("success", "Logged Out Successfully !"); // Display a success flash message
+    return res.redirect("/users/sign-in"); // Redirect to the sign-in page
   });
 };
 
-// forrget password page
+// Forget password page
 module.exports.forgetPasswordPage = function (req, res) {
-  return res.render("forget_password");
+  return res.render("forget_password"); // Render the forget password page view
 };
 
-// this will update the existing password, with the newly created password.
+// Updating the existing password
 module.exports.updatePassword = async function (req, res) {
   try {
     let user = await User.findOne({ email: req.body.email });
 
     if (!user) {
-      req.flash("error", "User does not exist. Please register");
-      return res.redirect("/users/signUp");
+      req.flash("error", "User does not exist. Please register"); // Display an error flash message
+      return res.redirect("/users/signUp"); // Redirect to the sign-up page
     }
 
     if (req.body.password == req.body.confirmPassword) {
-      req.flash("success", "Password Changed :)");
+      req.flash("success", "Password Changed :)"); // Display a success flash message
       user.password = req.body.password;
       await user.updateOne({ password: req.body.password });
-      return res.redirect("/users/sign-in");
+      return res.redirect("/users/sign-in"); // Redirect to the sign-in page
     }
 
-    return res.redirect("back");
+    return res.redirect("back"); // Redirect back to the previous page
   } catch (error) {
     // Handle the error appropriately
     console.error("Error in updatePassword:", error);
-    req.flash("error", "An error occurred while updating the password.");
-    return res.redirect("back");
+    req.flash("error", "An error occurred while updating the password."); // Display an error flash message
+    return res.redirect("back"); // Redirect back to the previous page
   }
 };
 
-// creating the session, basically for logging In
+// Logging in as an admin
 module.exports.loginAsAdmin = async function (req, res) {
   try {
 
     let user = User.findById(req.params.id);
 
-
-    if(!user){
-        req.flash("error", "User does not exits. Please registered before advancing.");
-        return res.redirect("back");
+    if (!user) {
+      req.flash("error", "User does not exist. Please register before advancing."); // Display an error flash message
+      return res.redirect("back"); // Redirect back to the previous page
     }
 
-    if(user.isAdmin){
-        req.flash("success", "Successfully logged in.");
-        return res.redirect("/");
+    if (user.isAdmin) {
+      req.flash("success", "Successfully logged in."); // Display a success flash message
+      return res.redirect("/"); // Redirect to the home page
+    }
+
+    if (req.body.adminPassCode != "Admin@123") {
+      req.flash("error", "You have entered the wrong passcode"); // Display an error flash message
     }
 
     user.isAdmin = true;
     await user.save();
 
-    req.flash("success", "Successfully logged in.");
-    return res.redirect("/");
+    req.flash("success", "Successfully logged in."); // Display a success flash message
+    return res.redirect("/"); // Redirect to the home page
 
   } catch (error) {
     // Handle the error appropriately
     console.error(error);
-    req.flash("error", "Something went wrong!");
-    return res.redirect("back");
+    req.flash("error", "Something went wrong!"); // Display an error flash message
+    return res.redirect("back"); // Redirect back to the previous page
   }
 };
