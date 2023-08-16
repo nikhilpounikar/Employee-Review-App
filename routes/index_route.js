162c61fb365339091bf992cfba68f76d27849a18
nikhilpounikar <nikhilpounikar@gmail.com>
2023-08-16 09:48:46 +0530

    routing name changes

--- routes/index_route.js
+++ routes/index_route.js
@@ -4,15 +4,17 @@ const homeController = require('../controllers/home_controller'); // requeiring 
 
 console.log(`router is loaded : {200}`);
 
+//This is initial route which decides the further routing.
+
 // It will redirect you to the home page
 router.get('/' , homeController.home);
 
 // all the requiest withe the suffix /userr, will require the user file, to compute
-router.use('/users' , require('./users'));
+router.use('/users' , require('./users_route'));
 // all the request with the suffix /admin , will require the admin file to compute
 router.use('/admin' , require('./admin'));
 
 // all the request with the suffix /reviews , will require the admin file to compute
-router.use('/reviews', require('./reviews'));
+router.use('/reviews', require('./reviews_route'));
 
 module.exports = router;
